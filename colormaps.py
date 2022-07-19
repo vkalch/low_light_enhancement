dd30a350d23e84cd535d3a2f0db4e8ef02a82d6c
avbalsam <avbalsam@gmail.com>
2022-07-19 11:25:41 -0400

    Add hot and cool colormap

--- colormaps.py
+++ colormaps.py
@@ -2,9 +2,11 @@ import cv2
 
 colormaps = [
     ('Jet', cv2.COLORMAP_JET),
-    ('HSV', cv2.COLORMAP_HSV),
-    ('Rainbow', cv2.COLORMAP_RAINBOW),
+    ('Rainbow 1', cv2.COLORMAP_HSV),
+    ('Rainbow 2', cv2.COLORMAP_RAINBOW),
     ('Grayscale', cv2.COLORMAP_BONE),
+    ('Red', cv2.COLORMAP_HOT),
+    ('Blue-Pink', cv2.COLORMAP_COOL)
 ]
 
 
