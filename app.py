5e4cf222c0d1a58bd76ff49c56b933b1ded3a463
avbalsam <avbalsam@gmail.com>
2022-07-26 01:12:26 -0400

    Add main.py to run app

--- app.py
+++ app.py
@@ -28,6 +28,12 @@ from low_signal_detector import LowSignalDetector as LSD
 
 app = Flask(__name__)
 
+# Set allowed extensions
+ALLOWED_EXTENSIONS = {'tiff', 'tif'}
+
+# Create a new instance of the LSD class to store user data
+LOW_SIGNAL_DETECTORS = list()
+
 
 def find_lsd_by_id(detector_id):
     for detector in LOW_SIGNAL_DETECTORS:
@@ -219,31 +225,3 @@ def algorithm():
     t.start()
 
     return render_template('submitted.html', user_id=lsd.ID)
-
-
-if __name__ == '__main__':
-    # Create folders to store user uploaded and enhanced images
-    enhanced_image_folder = os.path.join('static', 'enhanced_data')
-    upload_folder = os.path.join('static', 'uploads')
-    app.config['ENHANCED_FOLDER'] = enhanced_image_folder
-    app.config['UPLOAD_FOLDER'] = upload_folder
-
-    # Do not allow uploading files greater than 32 megabytes
-    app.config['MAX_CONTENT_LENGTH'] = 32 * 1000 * 1000
-
-    # Set allowed extensions
-    ALLOWED_EXTENSIONS = {'tiff', 'tif'}
-
-    # Make sure user upload and enhanced image folders exist
-    if not (os.path.exists(enhanced_image_folder)):
-        os.makedirs(enhanced_image_folder)
-
-    if not (os.path.exists(upload_folder)):
-        os.makedirs(upload_folder)
-
-    # Create a new instance of the LSD class to store user data
-    LOW_SIGNAL_DETECTORS = list()
-
-    # Start the flask app
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
